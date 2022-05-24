f224863b21b3a1d44eb1f41aec6327a290a2eda8
Oleg Freidkin <oleg.freidkin@gmail.com>
2022-05-25 02:02:04 +0300

    Добавьте файлы проекта.

---
+++ SFU25EF/AppContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SFU25EF
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Ваша строка подключения к БД");
+        }
+    }
+}
